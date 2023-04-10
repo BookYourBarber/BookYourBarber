7965ddb5c67b017574a4192263b58075cb348315
VeljkoSkrbic <veljko.skrbic2704@gmail.com>
2023-04-10 20:50:27 +0200

    Google map and barbers information

---
+++ frontend/customer_frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Asdf from './Components/Asdf'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Asdf/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
